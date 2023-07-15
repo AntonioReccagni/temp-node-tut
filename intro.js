6d3eea3ffe84fa29af2b80952a8e4fa003d71d18
Antonio RECCAGNI <antonio.reccagni@alten.it>
2023-07-15 10:47:18 +0200

    First commit

---
+++ intro.js
@@ -0,0 +1,11 @@
+const amount = 12
+
+if (amount < 12){
+    console.log('snmall number')
+}
+else{
+    console.log('large number')
+}
+
+console.log('hey,è la mia prima app node')
+
---
+++ lesson_module.js
@@ -0,0 +1,10 @@
+// Modules
+const names = require('./name')
+const sayHi = require('./utils')
+const data = require('./person')
+require('./minegranade')
+
+
+sayHi('Susan')
+sayHi(names.john)
+sayHi(names.peter)
